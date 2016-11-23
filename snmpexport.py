153cb84d47db074bec712064d24d96ee0f30e289
Christian Svensson <blue@cmd.nu>
2016-11-23 16:02:37 +0100

    export run without annotate works

--- snmpexport.py
+++ snmpexport.py
@@ -10,7 +10,7 @@ import snmpexporter.snmpimpl
 import snmpexporter.annotator
 
 
-def main(config_file, host, layer):
+def main(config_file, host, layer, annotate=True):
   with open(config_file, 'r') as f:
     config = yaml.safe_load(f.read())
   collections = config['collection']
@@ -19,17 +19,30 @@ def main(config_file, host, layer):
 
   # TODO(bluecmd): fix
   mibresolver = None
+
+  if not annotate:
+    logging.debug('Will not annotate')
+
   logging.debug('Initializing Net-SNMP implemention')
   snmpimpl = snmpexporter.snmpimpl.NetsnmpImpl()
   logging.debug('Constructing SNMP target')
   target = snmpexporter.target.SnmpTarget(host, layer, snmp_creds)
   logging.debug('Creating SNMP poller')
   poller = snmpexporter.poller.Poller(collections, overrides, snmpimpl)
+
+  logging.debug('Starting poll')
+  raw_data, timeouts, errors = poller.poll(target)
+
+  if not annotate:
+    for (oid, vlan), value in sorted(raw_data.items()):
+      print(str(vlan if vlan else '').ljust(5), oid.ljust(50), value)
+    return
+
   logging.debug('Creating result annotator')
   annotator = snmpexporter.annotator.Annotator(config, mibresolver)
 
-  logging.debug('Starting poll')
-  for result in annotator.annotate(poller.poll(target)):
+  logging.debug('Starting annotation')
+  for result in annotator.annotate(raw_data):
     print(result)
 
 
@@ -41,6 +54,8 @@ if __name__ == '__main__':
           help='config file to load', default='/etc/snmpexporter.yaml')
   parser.add_argument('--log-level', dest='log_level', type=str,
           help='log level', default='INFO')
+  parser.add_argument('--annotate', dest='annotate', default=False, const=True,
+          help='annotate the results', action='store_const')
   parser.add_argument('host', type=str, help='host to scrape')
   parser.add_argument('layer', type=str, help='layer to use for authentication')
   args = parser.parse_args()
@@ -54,4 +69,4 @@ if __name__ == '__main__':
   root.addHandler(ch)
   root.setLevel(logging.getLevelName(args.log_level))
 
-  main(args.config_file, args.host, args.layer)
+  main(args.config_file, args.host, args.layer, annotate=args.annotate)
